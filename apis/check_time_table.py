062c91ecfcb295c712219d85cc72c5a1a42944f0
bateni1380 <bateni1380@gmail.com>
2023-02-04 18:21:58 +0330

    Final version!

--- apis/check_time_table.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def show_mh_day_meetings(mh_id):
-    pass #zahra side
-    
-
-input = '''{ 
-    "mh_id":"123"
-        '''
--- apis/list_of_mh.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def mh_list(mh_id):
-    pass #zahra 
-    
-
-
-input = '''
-        '''
